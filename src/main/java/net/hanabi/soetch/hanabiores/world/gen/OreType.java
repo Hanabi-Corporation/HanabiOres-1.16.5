8cbd2f88b571550dddf13d226446e5c8b02e1571
Marceau Malégeant (Soetch) <soetchdev@gmail.com>
2022-02-24 19:43:38 +0100

    Updated ModID.

---
+++ src/main/java/net/hanabi/soetch/hanabiores/world/gen/OreType.java
@@ -0,0 +1,48 @@
+package net.hanabi.soetch.hanabiores.world.gen;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.common.util.Lazy;
+import net.hanabi.soetch.hanabiores.block.ModBlocks;
+
+public enum OreType {
+    LIRIUM(Lazy.of(ModBlocks.LIRIUM_ORE), 6, 15, 30),
+    AMATITE(Lazy.of(ModBlocks.AMATITE_ORE), 5, 10, 25),
+    MAYITH(Lazy.of(ModBlocks.MAYITH_ORE), 4, 5, 20);
+
+    private final Lazy<Block> block;
+    private final int maxVeinSize;
+    private final int minHeight;
+    private final int maxHeight;
+
+    OreType(Lazy<Block> block, int maxVeinSize, int minHeight, int maxHeight) {
+        this.block = block;
+        this.maxVeinSize = maxVeinSize;
+        this.minHeight = minHeight;
+        this.maxHeight = maxHeight;
+    }
+
+    public Lazy<Block> getBlock() {
+        return block;
+    }
+
+    public int getMaxVeinSize() {
+        return maxVeinSize;
+    }
+
+    public int getMinHeight() {
+        return minHeight;
+    }
+
+    public int getMaxHeight() {
+        return maxHeight;
+    }
+
+    public static OreType get(Block block) {
+        for(OreType ore : values()) {
+            if(block == ore.block) {
+                return ore;
+            }
+        }
+        return null;
+    }
+}
