3548b9ff85c70f050bd5003d6742d3b14d95f266
Marceau Malégeant (Soetch) <soetchdev@gmail.com>
2022-03-05 18:10:58 +0100

    Minor Changes.

--- src/main/java/net/hanabi/soetch/hanabiores/HanabiOres.java
+++ src/main/java/net/hanabi/soetch/hanabiores/HanabiOres.java
@@ -1,5 +1,13 @@
 package net.hanabi.soetch.hanabiores;
 
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.hanabi.soetch.hanabiores.block.ModBlocks;
+import net.hanabi.soetch.hanabiores.item.ModItems;
+import net.hanabi.soetch.hanabiores.util.Reference;
 import net.minecraft.block.Block;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
@@ -13,13 +21,6 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.hanabi.soetch.hanabiores.block.ModBlocks;
-import net.hanabi.soetch.hanabiores.item.ModItems;
-import net.hanabi.soetch.hanabiores.util.Reference;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Reference.MOD_ID)
